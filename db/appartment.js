59089794161dc525272f76a9df079b260f3ff25b
Menhem-byte <gehad.menhem@sait.edu.ca>
2022-07-24 11:04:44 -0600

    what ever

--- db/appartment.js
+++ db/appartment.js
@@ -1,6 +1,7 @@
 const knex =require('./dbconfig')
 const insertupdatedelete = require('./insertupdatedelete')
 const building=require('./building')
+const { reject } = require('lodash')
 
 // get all apartments in a building
 async function  appartments(building_Id){
@@ -33,16 +34,59 @@ async function  appartments(building_Id){
     }
 }
 
-//insert appartment according to the building Id
-async function  insertAppartments(data){
+//get a specific appartment
+async function appartment(data){
 
+    try{
+   
+     return new Promise(async(resolve,reject)=>{
+        let getBuilding= await building.building(data.BuildingId)
+      
+        if(getBuilding.length){
+            let appartment =await knex.select(
+              '*'
+            ).from('Appartment as A')
+            .where('A.AppNumber','=',data.AppNumber)
+            .where('A.BuildingId','=',data.BuildingId)
+
+          if(appartment.length){
+              resolve(appartment)
+          }
+          else{
+             resolve('no record found for appartment')
+          }
+            
+        }
+        else{
+            resolve('no building found')
+        }
+        })       
+    }catch(err){
+        console.log(err)
+    }
+}
+
+
+
+//insert appartment according to the building Id
+async function  insertAppartment(data){
    try{
     return new Promise(async(resolve,reject)=>{
         let getBuilding= await building.building(data.BuildingId)
-        console.log(getBuilding)
+      
         if(getBuilding.length){
-          let insertAppartment= await insertupdatedelete.insert('Appartment',data)
-          resolve(insertAppartment)
+            let conflict= await appartment(data)
+           
+            if(conflict.includes('no record found for appartment')){
+                let insertAppartment= await insertupdatedelete.insert('Appartment',data)
+                resolve(insertAppartment)
+            }
+            else{
+                let available=await appartments(data.BuildingId)
+                
+              resolve(available)
+            }
+         
         }
         else{
             resolve('building is not exist add an Appartment to an existing building')
@@ -54,9 +98,32 @@ async function  insertAppartments(data){
    }
 }
 
+//update appartment 
+async function  updateAppartment(data){
+
+    try{
+     return new Promise(async(resolve,reject)=>{
+         let getBuilding= await building.building(data.BuildingId)
+         console.log(getBuilding)
+         if(getBuilding.length){
+           let insertAppartment= await insertupdatedelete.update('Appartment','Id',data)
+           resolve(insertAppartment)
+         }
+         else{
+             resolve('building is not exist add an Appartment to an existing building')
+         }
+     })
+    }
+    catch(err){
+     console.log(err)
+    }
+ }
+
 
 module.exports=
 {
     appartments:appartments,
-    insertAppartments:insertAppartments
+    insertAppartment:insertAppartment,
+    updateAppartment:updateAppartment,
+    appartment:appartment
 }
--- routes/apiappartment.js
+++ routes/apiappartment.js
@@ -26,12 +26,49 @@ router.route('/appartments').get(async(req,res)=>{
     }
 })
 
+
+router.route('/appartment').get(async(req,res)=>{
+ console.log(req.query.AppNumber)
+  let data={AppNumber:req.query.AppNumber,BuildingId:req.query.BuildingId}
+   console.log(data)
+  if(data){
+    
+    let result = await DBappartment.appartment(data)
+    if(result){
+      res.status(200).send(result)
+    }
+    else{
+      res.status(500).send(result)
+    }
+    
+  }
+  else{
+      
+      res.status(400).send('query error')
+  }
+})
+
 /************************************************************
  *       Post method
  ************************************************************/
 
 router.route('/insertAppartment').post(async(req,res)=>{
-  DBappartment.insertAppartments(req.body).then(result =>{
+  DBappartment.insertAppartment(req.body).then(result =>{
+   
+    res.status(200).send(result)
+  })
+  .catch(err=>{
+    res.status(400).send(err)
+  })
+})
+
+
+/************************************************************
+ *       Put method
+ ************************************************************/
+
+ router.route('/updateAppartment').put(async(req,res)=>{
+  DBappartment.updateAppartment(req.body).then(result =>{
    
     res.status(200).send(result)
   })
